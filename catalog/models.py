c19a55ce3051075aa20f168b615d079fa72b0f2a
ArtemMikhal <141834394+ArtemMikhal@users.noreply.github.com>
2023-12-05 21:06:47 +0300

    Выполнено задание 5

--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.CharField(max_length=300, verbose_name='описание')
     preview = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
     category = models.ForeignKey('Category', on_delete=models.PROTECT)
-    price = models.BooleanField(verbose_name='цена за штуку')
+    price = models.IntegerField(verbose_name='цена за штуку')
     date_of_creation = models.DateField(verbose_name='дата создания')
     date_modification = models.DateField(verbose_name='дата последнего изменения')
 
@@ -22,7 +22,7 @@ class Product(models.Model):
 class Category(models.Model):
     category_name = models.CharField(max_length=100, verbose_name='наименование категории')
     description = models.CharField(max_length=300, verbose_name='описание')
-    created_at = models.CharField(max_length=20, verbose_name='created', **NULLABLE)
+
     def __str__(self):
         return f'{self.category_name} {self.description}'
 
