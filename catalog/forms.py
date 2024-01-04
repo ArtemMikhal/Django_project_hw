6eeb5e788ac913073ae99c9a556840267231e233
ArtemMikhal <141834394+ArtemMikhal@users.noreply.github.com>
2024-01-04 16:45:10 +0300

    Создал файл с форрмой

---
+++ catalog/forms.py
@@ -0,0 +1,15 @@
+﻿from django import forms
+
+from catalog.models import Product
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+        #fields = ('name', 'description' ) определенные атрибуты
+        #exclude = ('preview') для исключения какого-либо атрибута
+
+
