711e99a8719cc420e316467e4a6e83ec939bf211
ArtemMikhal <141834394+ArtemMikhal@users.noreply.github.com>
2024-01-23 17:31:49 +0300

    Скрыл пароли

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dotenv
+
+# Загрузка переменных среды из файла .env
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'HW_20.1',
         'USER': 'postgres',
-        'PASSWORD': '4242'
+        'PASSWORD': os.getenv('DB_PASSWORD')
     }
 }
 
@@ -140,12 +144,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'AMikhalchenkov'
-EMAIL_HOST_PASSWORD = 'nflzbbsvedjqtqcj'
-EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = False
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
